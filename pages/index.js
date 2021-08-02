68ded462d5b5aa99c699a84bec47425bcc97dbe5
Ankith Vaitla <ankithvaitla345@gmail.com>
2021-08-02 15:33:04 +0530

    SSR for popular movies in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,35 @@
 import Head from "next/head";
-import SearchBar from "../components/SearchBar";
-import { useState } from "react";
-import PopularMovies from "../components/PopularMovies";
+import Movie from "../components/Movie";
 
-export default function Home() {
+export default function Home({ movies }) {
   return (
     <div>
       <Head>
         <title>Movies</title>
       </Head>
-      <PopularMovies />
+      Popular Movies
+      {movies.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <Movie movie={movie} />
+          </div>
+        );
+      })}
     </div>
   );
 }
 
+// SSR popular movies
 export const getServerSideProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
-  );
-  const articles = await res.json();
+  const API_KEY = "41250625009617e91c5ad82fbd838793";
+  const BASE_URL = "https://api.themoviedb.org/3/";
+
+  const res = await fetch(`${BASE_URL}movie/popular/?api_key=${API_KEY}`);
+  const movies = (await res.json()).results;
 
   return {
     props: {
-      articles,
+      movies,
     },
   };
 };
