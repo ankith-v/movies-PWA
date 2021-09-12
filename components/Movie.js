22da9f3a32048fb4c26a569ff92051988851045e
Ankith Vaitla <ankithvaitla345@gmail.com>
2021-09-12 18:16:13 +0530

    movie title styled

--- components/Movie.js
+++ components/Movie.js
@@ -1,14 +1,23 @@
-import { Image, Link, Box } from "@chakra-ui/react";
+import { Image, Link, Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Movie = (props) => {
   return (
-    <Box padding="2">
+    <Box padding="1">
       <Link
         href={`https://www.themoviedb.org/movie/${props.movie.id}`}
         isExternal
       >
-        {props.movie.original_title}
+        <Text
+          maxW="60"
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow="ellipsis"
+          textAlign="center"
+          paddingBottom="0.5"
+        >
+          {props.movie.original_title}
+        </Text>
         <Image
           src={`https://image.tmdb.org/t/p/w400/${props.movie.poster_path}`}
           alt="#movie poster#"
